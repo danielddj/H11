621b47fd8fc047a177ae7aeb03bef3d4791f63fb
Ria <kristoffer.berg8@gmail.com>
2023-02-13 16:16:09 +0100

    Added additional tests.

--- src/main.test.ts
+++ src/main.test.ts
@@ -8,3 +8,31 @@ test("Square function", () => {
     const res = execute(the_global_environment, function_declaration + function_use);
     expect(res).toBe(120);
 });
+
+test("constant declaration and usage", () => {
+    const constant_declaration = "const x = 512; x;"
+
+    const res = execute(the_global_environment, constant_declaration);
+    expect(res).toBe(512);
+});
+
+test("conditional statement", () => {
+    const constant_declaration_a = "const x = 512;";
+    const constant_declaration_b = "const x = 200;";
+    const conditional_statement = "x > 300 ? 1 : 2;"; 
+
+    const res_a = execute(the_global_environment, constant_declaration_a + 
+        conditional_statement);
+    const res_b = execute(the_global_environment, constant_declaration_b + 
+        conditional_statement);
+    expect(res_a).toBe(1);
+    expect(res_b).toBe(2);
+});
+
+test("function declaration and call", () => {
+    const func = "function foo() { return 32; } "
+    const func_call = "foo();";
+
+    const result = execute(the_global_environment, func + func_call);
+    expect(result).toBe(32);
+});
