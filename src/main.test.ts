73aff5eb9a43ca7e0e747967aa19d611de005b1b
Ria <kristoffer.berg8@gmail.com>
2023-02-14 12:33:52 +0100

    Fixed name for test case 1.

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,7 +1,7 @@
 import { is_boolean, set_head, is_pair, list_ref, apply_in_underlying_javascript, pair, stringify, is_null, error, math_abs, math_PI, math_E, display, map, accumulate, length, parse, append, head, list, tail, List, Pair} from 'sicp';
 import { evaluate, the_global_environment, execute } from "./main";
 
-test("Square function", () => {
+test("Factorial function", () => {
     const function_declaration = "function factorial(n) { return n < 2 ? n : n * factorial(n - 1); }";
     const function_use = "factorial(5);";
 
